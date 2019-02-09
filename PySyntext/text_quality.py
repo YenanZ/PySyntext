1b95e6cca4f268d7fea94dd9ba8a7c7ea9261fe6
Harjyot Kaur <harjyotkaur.1409@gmail.com>
2019-02-09 09:31:39 -0800

    Added docstring for text_quality

--- PySyntext/text_quality.py
+++ PySyntext/text_quality.py
@@ -1,1 +1,63 @@
-## function TexQuality
+## function text_quality
+
+"""
+
+Created on February, 2019
+
+
+@author: Harjyot Kaur
+
+
+Implementation of text_quality function in the PySyntext package.
+
+"""
+
+# load packages
+import pandas as pd
+
+
+def text_quality(string):
+
+
+
+    """
+    
+    Check quality of the string in terms of
+    spelling errors and toxicity content. 
+    
+    The function performs necessary cleaning 
+    on the input string. 
+    
+    Comparison is done with pre-existing list of
+    exhaustive english words to calculate the spelling errors in the string.
+    Comparison is done with pre-existing list of
+    exhaustive toxic-english words to calculate the toxicity in the string.
+    
+   
+    Parameters
+
+    ----------
+
+    string : str
+
+        input string ti be analyzed
+        given by the user
+
+
+    Returns
+
+    -------
+
+    dataframe
+
+        First column contains proportion of spelling errors 
+        in the input contains and the second column stores 
+        toxicity in the the input string.
+
+    """
+
+
+
+    output = pd.DataFrame()
+
+    return output
