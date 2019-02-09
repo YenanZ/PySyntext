d6be4c4eedc17dd7fec0add4861dfff1240e2925
Yenan Zhang <yenanz@outlook.com>
2019-02-09 14:28:38 -0800

    test and doc for text_grams

--- PySyntext/text_grams.py
+++ PySyntext/text_grams.py
@@ -1,1 +1,53 @@
 ## function TextGrams
+
+"""
+
+Created on 09 February, 2019
+
+
+@author: Yenan Zhang
+
+
+Implementation of text_grams function
+
+"""
+
+# load packages
+import pandas as pd
+
+
+def text_grams(string, k, n, stop_remove, lemitize, remove_punctuation,remove_number,case_sensitive,gibberish_remove):
+
+
+
+    """
+    
+    This function produces DataFrame as an output containing lists of top 5 ngrams.
+
+    Parameters
+
+    ----------
+
+    string
+    k
+    n
+    stopword_remove
+    lemitize
+    remove_function
+    remove_number
+    case_sensitive
+    gibberish_remove
+
+
+    Returns
+
+    -------
+
+    dataframe
+    
+    each column is the top n ngrams"""
+    
+    return ngrams_df
+
+    
+
--- test/test_text_grams.py
+++ test/test_text_grams.py
@@ -1,1 +1,19 @@
 ## test for TextGrams
+
+import pytest
+import pandas as pd
+import os
+import sys
+import numpy
+sys.path.insert(0, os.path.abspath(".."))
+from PySyntext.text_grams import text_ngrams
+
+ex_passage = "Today is a beautiful day which is perfect for going to the beach, the weather is nice and the sky is blue."
+
+def test_output(x):
+    ex_output=text_grams(x)
+    assert type(ex_output) == type(pd.DataFrame())
+    assert type(ex_output[,1])==type(list())
+    
+
+    
