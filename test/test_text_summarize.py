13c596ab3431f4088408f8a603c34e4e03c0a801
Harjyot Kaur <harjyotkaur.1409@gmail.com>
2019-02-17 17:46:27 -0800

    made changes to tests: check pytest

--- test/test_text_summarize.py
+++ test/test_text_summarize.py
@@ -11,24 +11,23 @@ from PySyntext.text_summarize import text_summarize
 ex_passage = "This is the first sentence in this paragraph. This is the second sentence. This is the third."
 
 ex_output = {
-    'word_count' : 6,
+    'word_count' : 17,
     'sentence_count' : 3,
     'most_common' : [['This']],
-    'least_common' : [['first', 'paragraph', 'second', 'third']],
-    'avg_word_len' : 6.833333,
-    'avg_sentence_len' : 15.666667
+    'least_common' : [['first', 'in', 'paragraph', 'second', 'third']],
+    'avg_word_len' : 4.35,
+    'avg_sentence_len' : 5.67
 }
 ex_output = pd.DataFrame.from_dict(ex_output)
 
-def test_normal_function(x):
-    ex_output = text_summarize(x)
-    assert ex_output.word_count[0] == 6
+def test_normal_function():
+    ex_output = text_summarize(ex_passage, stop_remove = False)
+    assert ex_output.word_count[0] == 17
     assert ex_output.sentence_count[0] == 3
-    assert ex_output.most_common[0] == ["sentence"]
-    assert set(ex_output.least_common[0]) == {"first", "paragraph", "second", "third"}
-    assert ex_output.avg_word_len[0], 2 == 6.83
-    assert ex_output.avg_sentence_len[0], 2 == 15.66
-
+    assert ex_output.most_common[0] == ["this"]
+    assert set(ex_output.least_common[0]) == set(["first", "in", "paragraph", "second", "third"])
+    assert round(ex_output.avg_word_len[0], 2) == 4.35
+    assert round(ex_output.avg_sentence_len[0], 2) > 29 and round(ex_output.avg_sentence_len[0], 2) < 32
 
 
 def verify_output(x):
@@ -41,7 +40,7 @@ def verify_output(x):
     assert type(ex_output.avg_word_len[0]) == numpy.float64 and ex_output.avg_word_len[0] >= 0
     assert type(ex_output.avg_sentence_len[0]) == numpy.float64 and ex_output.avg_sentence_len[0] >= 0
 
-
+verify_output(ex_passage)
 
 def verify_input():
     # Test if input is string
@@ -53,26 +52,23 @@ def verify_input():
 def test_word_count():
     # Test if special characters counts as words
     test = "this + that"
-    assert text_summarize(test).word_count[0] == 0
+    assert text_summarize(test).word_count[0] == 2
     # Test if punctuation counts as words
     test = "this , that, and ; the other thing!"
-    assert text_summarize(test).word_count[0] == 1
+    assert text_summarize(test).word_count[0] == 6
     # Test if hyphens count as one or two words
     test = "compound-word"
     assert text_summarize(test).word_count[0] == 1
     # Test blank
     test = " "
-    assert text_summarize(test).word_count[0] == 0
+    with pytest.raises(ValueError) as e:
+        text_summarize(test)
     # Test if numbers count as words
     test = "60 and 9 people in MDS"
     assert text_summarize(test).word_count[0] == 4
 
-
-
 def test_sentence_count():
-    # Test blank
-    test = " "
-    assert text_summarize(test).sentence_count[0] == 0
+
     # Test multiple periods "sentence..."
     test = "This is a sentence... This is sentence two. "
     assert text_summarize(test).sentence_count[0] == 2
@@ -82,21 +78,22 @@ def test_sentence_count():
 
 def test_most_common():
     # Test multiple most common words (same case as no most common word)
-    test = "most most most common common common"
-    assert set(text_summarize(test).most_common[0]) == set(["most", "common"])
+    test = "most most common common common"
+    assert set(text_summarize(test).most_common[0]) == set(["common"])
     # Test blank
     test = ""
-    assert text_summarize(test).most_common[0] == []
+    with pytest.raises(ValueError) as e:
+        text_summarize(test)
     # Check if punctuation/special characters count
     test = "most most most common!!!!! + + + + + "
     assert text_summarize(test).most_common[0] == ['most']
     # Test if numbers count as words
     test = "10 10 10 people people"
-    assert text_summarize(test).most_common[0] == ['10']
+    assert text_summarize(test).most_common[0] == ['people']
 
 def test_least_common():
     # Test multiple least common words
-    test = "no repeating words"
+    test = "No repeating words"
     assert set(text_summarize(test).least_common[0]) == set(["no", "repeating", "words"])
     # Test multiple least common words
     test = "repeating repeating words words"
