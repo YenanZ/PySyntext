6cb4cec6fb10d2904d879efeb68ceae3cdfe911f
Alexander Pak <pak.alexh@gmail.com>
2019-03-03 00:47:02 -0800

    Updated tests for coverage

--- test/test_text_grams.py
+++ test/test_text_grams.py
@@ -26,59 +26,62 @@ ex_output
 
 # test for functionality
 def test_normal_function(x, k, n):
+    x = "Today is a sunny day. We should go to a beach on this sunny day"
+    k=1
+    n=[2]
     ex_output = text_grams(x, k, n)
     assert ex_output["2gram"][0] ==  sample_out['2gram'][0]
     assert  len(ex_output['2gram'][0]) == n[0]
     assert  ex_output.shape[0] == sample_out.shape[0]
-test_normal_function(x, k, n)
+#test_normal_function(x, k, n)
 
 # test datatype inside output dataframe
 def verify_output(x, k, n):
+    x = "Today is a sunny day. We should go to a beach on this sunny day"
+    k=1
+    n=[2]
     ex_output = text_grams(x, k, n)
     assert type(ex_output) == type(sample_out)
     assert type(ex_output['2gram'][0]) == tuple
     assert type(ex_output['Number of Instances'][0]) == numpy.int64
     
-verify_output(x, k, n)
+#verify_output(x, k, n)
 
 # test sentence endings (?, !, .)
 def verify_sentence_endings():
     ex = "Wherefore art thou Romeo! Wherefore art thou Romeo. Wherefore art thou Romeo?"
     ex_output = text_grams(ex)
     assert ex_output.shape[0] != 4 # If it split sentences properly, (Romeo, Wherefore) will not be a gram combination
-verify_sentence_endings()
+#verify_sentence_endings()
 
 # test stop remove
 def verify_stop_remove():
     ex = "This is a sentence. This is also a sentence. This is also a sentence."
     ex_output = text_grams(ex, stop_remove = False)
     assert ex_output.shape[0] > 1 # If stopwords are removed, only (also, sentence) will be considered
-verify_stop_remove()
+#verify_stop_remove()
 
 # test punctuation
 def verify_remove_punctuation():
     ex = "This is / a sentence. This is / also a sentence. This is / also a sentence."
     ex_output = text_grams(ex, remove_punctuation = False)
     assert ex_output.shape[0] > 1 # If punctuation is removed, only (also, sentence) will be considered
-verify_remove_punctuation()
+#verify_remove_punctuation()
 
 # test remove number
 def verify_remove_number():
     ex = "This is 123 a sentence. This is 123 also a sentence. This is 123 also a sentence."
     ex_output = text_grams(ex, remove_number = False)
     assert ex_output.shape[0] > 1 # If numbers is removed, only (also, sentence) will be considered
-verify_remove_number()
+#verify_remove_number()
 
 # test case sensitive
 def verify_case_sensitive():
     ex = "Hey Guys. Hey Guys. hey guys. hey guys."
     ex_output = text_grams(ex, case_sensitive = True)
     assert ex_output.shape[0] > 1 # If case_sensitive is true, only (hey, guys) will be considered
-verify_case_sensitive()
+#verify_case_sensitive()
 
-def test_passes():
-    with pytest.raises(Exception) as e_info:
-        x = 1 / 0
 
 # test invalid values of k
 def verify_k():
@@ -89,7 +92,7 @@ def verify_k():
     with pytest.raises(ValueError) as e:
         text_grams(ex, k = -1)
     assert str(e.value) == "k must be 0 or greater"
-verify_k()
+#verify_k()
 
 # test invalid values of n
 def verify_n():
@@ -101,14 +104,14 @@ def verify_n():
     with pytest.raises(ValueError) as e:
         text_grams(ex, n = [-1])
     assert str(e.value) == "Values of n must be greater than 0"
-verify_n()
+#verify_n()
 
 # test if sentence length is smaller than n
 def verify_n_larger_than_sentence():
     ex = "short sentence. short sentence."
     ex_output = text_grams(ex, n = [3])
     assert ex_output.shape[0] == 0 # If n is larger than sentence length, dataframe should be empty
-verify_n_larger_than_sentence()
+#verify_n_larger_than_sentence()
 
 # test to verify if function raises for invalid input
 def verify_input1():
@@ -121,7 +124,7 @@ def verify_input1():
     with pytest.raises(ValueError) as e:
         text_grams(text)
     assert str(e.value) == "Input must be a string"
-verify_input1()
+#verify_input1()
 
 # test to verify if function raises for invalid input
 def verify_input2():
@@ -135,4 +138,4 @@ def verify_input2():
     with pytest.raises(ValueError) as e:
         text_grams(text)
     assert str(e.value) == "Input text is empty."
-verify_input2()
+#verify_input2()
